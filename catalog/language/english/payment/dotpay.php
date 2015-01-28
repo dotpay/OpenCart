2b0b48586c93aa123332948e6f424526b3e6fd7a
Mateusz Job <mateusz.job@akra.net>
2015-01-28 10:19:48 +0100

    create structure of module

---
+++ catalog/language/english/payment/dotpay.php
@@ -0,0 +1,12 @@
+<?php
+$_['text_title'] = 'Transferuj.pl';
+$_['text_order'] = 'Order no ';
+$_['text_payment_overpay'] = 'Order overpaid. Amount paid: ';
+$_['text_payment_surcharge'] = 'Order underpaid. Amount paid: ';
+$_['text_payment_tr_id'] = 'Transaction ID: ';
+$_['text_incorrect_md5sum'] = 'MD5 sum incorrect!';
+$_['text_transferuj_redirect'] = '<br /><br />You are redirected to pay using '.$_['text_title'].'<br /><br />If it does not happen automatically use this button:<br /><br />';
+$_['text_transferuj_redirect_btn'] = 'Pay using Transferuj.pl!';
+$_['text_bank_choice'] = 'Choose bank: ';
+$_['text_accept_terms'] = 'I accept <a href="https://secure.transferuj.pl/regulamin.pdf">terms</a>: ';
+?>
